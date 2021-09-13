bbc1cfa686c3eefc1767706ddb10abed3e6e5f9d
Manikandan <xqsit94@gmail.com>
2021-09-13 17:10:02 +0530

    Adsense tag

--- src/components/parts/AppSidebar.vue
+++ src/components/parts/AppSidebar.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="w-full flex flex-col my-4">
-    <p class="text-xl font-semibold pb-5">Sidebar</p>
-    <p class="pb-2">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mattis
-      est eu odio sagittis tristique. Vestibulum ut finibus leo. In hac
-      habitasse platea dictumst.
-    </p>
+    <p class="text-xl font-semibold pb-5">Ads</p>
+    <p class="pb-2"></p>
   </div>
 </template>
